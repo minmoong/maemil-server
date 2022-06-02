c5ce444b3934206918562e2dcfb5d1e4c9677bcb
minmoong <kim774634@gmail.com>
2022-06-03 01:21:08 +0900

    1차 배포 마무리

--- db/client.ts
+++ db/client.ts
@@ -1,10 +1,13 @@
 import { Client } from 'https://deno.land/x/mysql@v2.10.2/mod.ts'
+import { config } from 'https://deno.land/x/dotenv@v3.2.0/mod.ts'
+
+const env = config()
 
 const client = await new Client().connect({
-    hostname: 'devminmoong.cafe24.com',
-    username: 'devminmoong',
-    db: 'devminmoong',
-    password: 'Minmoongsdft8753'
+    hostname: env.HOSTNAME,
+    username: env.USERNAME,
+    db: env.DB,
+    password: env.PASSWORD
 })
 
 export default client
